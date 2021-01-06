351fce450e5c9ee5affb3262876e7dba3e170873
lorakis <karol.cieslawski@gmail.com>
2021-01-07 00:49:17 +0100

    lista10.1

--- lista10/zad2/zad2.c
+++ lista10/zad2/zad2.c
@@ -43,13 +43,13 @@ void timer_init()
   // WGM1  = 1110 -- fast PWM top=ICR1
   // CS1   = 101  -- prescaler 1024
   // ICR1  = 15624
-  // częstotliwość 16e6/(1024*(1+31)) = 500 Hz
+  // częstotliwość 16e6/(1024*(9+1)) = 1562 Hz
   // 16 -> 1kHz
   // wzór: datasheet 20.12.3 str. 164
-  ICR1 = 10230;
+  ICR1 = 16;
   TCCR1A = _BV(COM1A1);
-  TCCR1B = _BV(WGM13) | _BV(CS10);
-  TIMSK1 = _BV(ICIE1) | _BV(TOIE1);
+  TCCR1B = _BV(WGM13) | _BV(CS10) | _BV(CS12);;
+  TIMSK1 = _BV(ICIE1) | _BV(TOIE1); // odpala przerwania
   // ustaw pin OC1A (PB1) jako wyjście
   DDRB |= _BV(PB1);
 }
@@ -58,46 +58,47 @@ void timer_init()
 void adc_init()
 {
   ADMUX   = _BV(REFS0); // referencja AVcc, wejście ADC0
-  DIDR0   = _BV(ADC0D); // wyłącz wejście cyfrowe na ADC0
+  // DIDR0   = _BV(ADC0D); // wyłącz wejście cyfrowe na ADC0
   // częstotliwość zegara ADC 125 kHz (16 MHz / 128)
   ADCSRA  = _BV(ADPS0) | _BV(ADPS1) | _BV(ADPS2); // preskaler 128
   ADCSRA |= _BV(ADIE); // ADC interrupt enable
   ADCSRA |= _BV(ADEN); // włącz ADC
 }
-int8_t interrupt_type = 0;
-uint16_t ovf_val, capt_val;
+uint16_t capt, ovf, typ;
 
-ISR(TIMER1_OVF_vect)
+ISR(TIMER1_CAPT_vect) // tranzystro zamknięty
 {
-	ADMUX |= _BV(MUX0);
-	ADCSRA |= _BV(ADSC);
-	interrupt_type = 1;
+  typ = 1;
+  ADMUX |= _BV(MUX0); // przełączamy na ADC1
+  ADCSRA |= _BV(ADSC);
 }
 
-ISR(TIMER1_CAPT_vect)
+ISR(TIMER1_OVF_vect) // tranzystor otwarty
 {
-	interrupt_type = 2;
-	ADMUX |= _BV(MUX0);
-	ADCSRA |= _BV(ADSC);
+  typ = 2;
+  ADMUX |= _BV(MUX0); // przełączamy na ADC1
+  ADCSRA |= _BV(ADSC); // odpalamy konwersję, po konwersji odpala sie przerwanie adc
 }
 
 ISR(ADC_vect)
 {
-	if (interrupt_type == 1) {
-		ovf_val = ADC;
-		interrupt_type = 3;
-		ADMUX &= ~_BV(MUX0);
-		ADCSRA |= _BV(ADSC);
-	} else if (interrupt_type == 2) {
-		capt_val = ADC;
-		interrupt_type = 3;
-		ADMUX &= ~_BV(MUX0);
-		ADCSRA |= _BV(ADSC);
-	} else if (interrupt_type == 3) {
-		OCR1A = ADC * 10;
-	}
+  if(typ == 1)
+  {
+    ovf = ADC;
+    typ = 3;
+    ADMUX &= ~_BV(MUX0); // przełączamy na ADC0
+    ADCSRA |= _BV(ADSC);
+  } else if(typ == 2)
+  {
+    capt = ADC;
+    typ = 3;
+    ADMUX &= ~_BV(MUX0);
+    ADCSRA |= _BV(ADSC);
+  } else if(typ == 3)
+  {
+    OCR1A = ADC / 128;
+  }
 }
-
 
 
 FILE uart_file;
@@ -112,8 +113,9 @@ int main()
   adc_init();
 	timer_init();
 	sei();
+
   while(1)
   {
-		printf("Zmierzono ovf: %"PRIu16"mv | capt: %"PRIu16"mv\r\n", ovf_val * (5000 / 1024), capt_val * (5000 / 1024));
-	}
+    printf("ovf: %"PRIu16"mV     capt: %"PRIu16"mV\r\n", ovf * (5000 / 1024), capt * (5000 / 1024));
+  }
 }
